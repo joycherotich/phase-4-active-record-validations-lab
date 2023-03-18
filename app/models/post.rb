7241ee9a2a7a1cbcab5156c70937e70377aaa55e
joy <letimjoy7@gmail.com>
2023-03-19 02:08:35 +0300

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, format: { with: /(Won't Believe|Secret|Top \d|Guess)/i, message: "must be clickbait-y" }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
 end
